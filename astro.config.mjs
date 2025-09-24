e035170f515197985fa4be5037c41cd619f53f2d
optanwar <optanwar10@gmail.com>
2025-09-24 23:25:10 +0530

    add sanity.io for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import sanity from "@sanity/astro";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
+  integrations: [sanity({
+      projectId: 'q7yn2bfx',
+      dataset: 'production',
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
+      studioBasePath: '/studio',
+      stega: {
+        studioUrl: "/studio",
+      },
+    }),react()],
 });
---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+// ./src/env.d.ts
+/// <reference types="astro/client" />
+/// <reference types="@sanity/astro/module" />
+
+PUBLIC_SANITY_PROJECT_ID:"",
+PUBLIC_SANITY_DATASET:"",
+
