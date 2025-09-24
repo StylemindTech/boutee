1cd7e88b05805f275549d21c25556109864554d3
optanwar <optanwar10@gmail.com>
2025-09-25 01:54:49 +0530

    Fixed server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
-import sanity from '@sanity/astro';
 
-// ✅ Use the server adapter for SSR
-import vercel from '@astrojs/vercel/server';
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // important for SSR
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [
-    sanity({
+
+  integrations: [sanity({
       projectId: 'q7yn2bfx',
       dataset: 'production',
-      useCdn: true,
-      apiVersion: "2025-01-28",
+      useCdn: true, // See note on using the CDN
+      apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
       studioBasePath: '/studio',
       stega: {
-        // Use absolute URL to avoid SSR crashes
-        studioUrl: "https://boutee.vercel.app/studio",
+        studioUrl: "/studio",
       },
-    }),
-    react()
-  ],
-  adapter: vercel(),
+    }),react()],    
+    
 });
