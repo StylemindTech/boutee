e927dad19c0da85662a17b8ed7b280616a97ef82
optanwar <optanwar10@gmail.com>
2025-09-25 15:38:02 +0530

    Fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,20 +11,20 @@ import vercel from '@astrojs/vercel';
 // import vercel from '@astrojs/vercel/server';
 // https://astro.build/config
 export default defineConfig({
-  //  output: "server", // important for SSR
+   output: "server", // important for SSR
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [sanity({
-      projectId: 'cvbiknat',
+      projectId: 'q7yn2bfx',
       dataset: 'production',
       useCdn: true, // See note on using the CDN
       apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
       studioBasePath: '/studio',
       stega: {
-        // studioUrl: "/studio",
-        studioUrl: "https://boutee.vercel.app/studio",
+        studioUrl: "/studio",
+        // studioUrl: "https://boutee.vercel.app/studio",
       },
     }),react()],    
      adapter: vercel(),
