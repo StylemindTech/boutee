bf832ea45222e9e65c2a2d271b4928dd2e3e7e4d
optanwar <optanwar10@gmail.com>
2025-09-27 22:33:01 +0530

    Fix .env issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,14 @@ import react from '@astrojs/react';
 import sanity from "@sanity/astro";
 
 import vercel from '@astrojs/vercel';
+import { loadEnv } from "vite";
+
+// Load environment variables from .env file
+const { PUBLIC_SANITY_PROJECT_ID } = loadEnv(process.env.PUBLIC_SANITY_PROJECT_ID, process.cwd(), "");
+const { PUBLIC_SANITY_DATASET } = loadEnv(process.env.PUBLIC_SANITY_DATASET, process.cwd(), "");
+
+console.log("Sanity Project ID:", PUBLIC_SANITY_PROJECT_ID);
+console.log("Sanity Dataset:", PUBLIC_SANITY_DATASET);
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,8 +25,8 @@ export default defineConfig({
   },
 
   integrations: [sanity({
-      projectId: 'we90e4mg',
-      dataset: 'production',
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
       useCdn: true, // See note on using the CDN
       apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
       studioBasePath: '/studio',
--- sanity.config.ts
+++ sanity.config.ts
@@ -3,8 +3,8 @@ import { structureTool } from "sanity/structure";
 import { schema } from "./src/sanity/schemaTypes";
 
 export default defineConfig({
-  projectId: import.meta.env.SANITY_PROJECT_ID, // server-only
-  dataset: import.meta.env.SANITY_DATASET,     // server-only
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID, // server-only
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET,     // server-only
   plugins: [
     structureTool(),
   ],
