74534ac2149313674b48ee16d529f14f38e0c82f
optanwar <optanwar10@gmail.com>
2025-09-27 21:50:15 +0530

    add .env

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,8 @@ import sanity from "@sanity/astro";
 
 import vercel from '@astrojs/vercel';
 import { loadEnv } from "vite";
-const { PUBLIC_SANITY_PROJECT_ID } = loadEnv(process.env.PUBLIC_SANITY_PROJECT_ID, process.cwd(), "");
-const { PUBLIC_SANITY_DATASET } = loadEnv(process.env.PUBLIC_SANITY_DATASET, process.cwd(), "");
+const { SANITY_PROJECT_ID } = loadEnv(process.env.SANITY_PROJECT_ID, process.cwd(), "");
+const {SANITY_DATASET } = loadEnv(process.env.SANITY_DATASET, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,8 +20,8 @@ export default defineConfig({
   },
 
   integrations: [sanity({
-      projectId: PUBLIC_SANITY_PROJECT_ID,
-      dataset: PUBLIC_SANITY_DATASET,
+      projectId: SANITY_PROJECT_ID,
+      dataset: SANITY_DATASET,
       useCdn: true, // See note on using the CDN
       apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
       studioBasePath: '/studio',
