4350eec70cd788f3cdd53e597b3e989f09dea946
Billy <billy@boutee.co.uk>
2025-10-01 22:08:20 +0300

    Remove .vercel output folder and add to gitignore

--- .vercel/output/_functions/chunks/page-ssr_C231QJuk.mjs
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"2025-01-10","projectId":undefined,"dataset":undefined,"useCdn":true,"stega":{"studioUrl":"\u002Fstudio"}}
-          );
-
-globalThis.sanityClient = sanityClient;
-
-export { sanityClient as s };
